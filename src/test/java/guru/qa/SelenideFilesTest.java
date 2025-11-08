797180e7b02a667feb87b415b7bf2d8b75a01785
Inworker <31877506+Inworker@users.noreply.github.com>
2025-11-09 00:58:22 +0300

    fix

--- src/test/java/guru/qa/SelenideFilesTest.java
+++ src/test/java/guru/qa/SelenideFilesTest.java
@@ -1,22 +1,37 @@
 package guru.qa;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideFilesTest {
     @Test
-    void downloadFileTest() throws Exception
-    {
-        open("https://github.com/junit-team/junit-framework/blob/main/README.md");
-        File download = $(".react-blob-header-edit-and-raw-actions [href*='/main/README.md']").download();
-        System.out.println();
-        InputStream inputStream = new FileInputStream(download);
+    void downloadFileTest() throws Exception {
+        open("https://github.com/junit-team/junit5/blob/main/README.md");
+        File downloaded =
+                $(".react-blob-header-edit-and-raw-actions [href*='main/README.md']")
+                        .download();
+
+        try (InputStream is = new FileInputStream(downloaded)) {
+            byte[] data = is.readAllBytes();
+            String dataAsString = new String(data, StandardCharsets.UTF_8);
+            Assertions.assertTrue(dataAsString.contains("Contributions to JUnit 5 are both welcomed and appreciated"));
+        }
+    }
+
+    @Test
+    void uploadFileTest() {
+        open("https://fineuploader.com/demos.html");
+        $("input[type='file']").uploadFromClasspath("AtomicHeart_sample.jpg");
+        $(".qq-file-name").shouldHave(text("AtomicHeart_sample.jpg"));
     }
 
 }
